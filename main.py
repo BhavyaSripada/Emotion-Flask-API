6e276e615c8b4ae3618f44266ec69dba297bb3a7
BhavyaSripada <bhavya.sripada@gmail.com>
2024-07-26 12:15:48 +0530

    Third Commit

--- main.py
+++ main.py
@@ -36,5 +36,5 @@ def emotion_detection_task(img):
         socketio.emit('emotion_result', {'emotions': 'No emotion detected'})
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
-
+    socketio.run(app, debug=True, host='0.0.0.0', port=5000, allow_unsafe_werkzeug=True)
+   
